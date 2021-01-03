de37a8b18da51f0367f61f8af3bf350943b333ed
Samuel Isaacson <samuel.b.isaacson@gmail.com>
2021-01-02 19:54:42 -0500

    Added notes on unique_ptr and std::function.

--- general_notes.cc
+++ general_notes.cc
@@ -128,6 +128,57 @@ std::function<int()> make_counter() {
     return [=]() mutable -> int { return count++; };
 }
 
+// The type std::unique_ptr<T, void (*)(T *)> is rarely what we
+// want. Instead, we should define a type with operator(): then the
+// destructor function isn't stored in the unique_ptr, and the
+// unique_ptr can be default-constructed. (We pay the cost of the
+// destructor in bloated code instead.) We can do that with a closure
+// or an explicit class. Note that C++ has some odd rules that make
+// working with zero-size types difficult. All objects have a minimum
+// size of a single byte (AFAIK) and all instances of an object must
+// have unique addresses. Fields that are zero-sized get upgraded to a
+// single byte (plus whatever is needed for padding), but classes that
+// inherit from a zero-sized type don't reserve any space for the
+// parent.
+
+template <typename T> struct FreeDeleter {
+    void operator()(T *ptr) { free(static_cast<void *>(ptr)); }
+};
+
+TEST(TypeErasure, UniquePtr) {
+    std::unique_ptr<void, void (*)(void *)> p1(malloc(4), free);
+    EXPECT_EQ(sizeof(void *) + sizeof(void (*)(void *)), sizeof(p1));
+    // The following fails to compile:
+    //
+    //     std::unique_ptr<void, void (*)(void *)> q;
+    //
+    // We have to say q(nullptr, free), which makes using this type in
+    // a collection very challenging.
+    //
+    // We can use a type-erased type (which is more appropriate than a
+    // function pointer---it's roughly the equivalent of Box<dyn
+    // FnOnce ...> in this case). Surprisingly, we can default-construct one:
+    std::unique_ptr<void, std::function<void(void *)>> p2(malloc(4), free);
+    EXPECT_EQ(sizeof(void *) + sizeof(std::function<void(void *)>), sizeof(p2));
+    std::unique_ptr<void, std::function<void(void *)>> p2_empty;
+    // Note that std::function<void(void *)> is much larger than a
+    // function pointer.
+
+    std::unique_ptr<int, FreeDeleter<int>> p3(
+        static_cast<int *>(malloc(sizeof(int))));
+    // I don't know if the standard requires the following assertion
+    // to be true, but it is:
+    EXPECT_EQ(1, sizeof(FreeDeleter<int>));
+    EXPECT_EQ(sizeof(int *), sizeof(p3));
+    std::unique_ptr<int, FreeDeleter<int>> p3_empty;
+
+    auto deleter = [](int *p) -> void { free(static_cast<void *>(p)); };
+    EXPECT_EQ(1, sizeof(deleter));
+    std::unique_ptr<int, decltype(deleter)> p4(
+        static_cast<int *>(malloc(sizeof(int))), deleter);
+    EXPECT_EQ(sizeof(int *), sizeof(p4));
+}
+
 TEST(Closures, Mutable) {
     auto c0 = make_counter();
     auto c1 = make_counter();
@@ -167,6 +218,55 @@ TEST(Spans, Example) {
     int primes[5] = {2, 3, 5, 7, 11};
     EXPECT_EQ(28, sum(primes)); // spans can be constructed from
                                 // C-style arrays
+}
+
+// invoke and bind, and function in the standard library make working
+// with function-like objects easier.
+
+class ClassWithVirtualFunction {
+  public:
+    ClassWithVirtualFunction(int x) : x_(x) {}
+    virtual int f(int a, int b) { return a * x_ + b; }
+
+  protected:
+    int x_;
+};
+
+class ChildClassWithVirtualFunction : public ClassWithVirtualFunction {
+  public:
+    ChildClassWithVirtualFunction(int x) : ClassWithVirtualFunction(x) {}
+    int f(int a, int b) override { return 10 * (a * x_ + b); }
+};
+
+TEST(Functional, PointerToMemberFunction) {
+    using RawMemberFunctionPointer =
+        int (ClassWithVirtualFunction::*)(int, int);
+    ChildClassWithVirtualFunction obj(10);
+    RawMemberFunctionPointer ptr = &ClassWithVirtualFunction::f;
+    // C++ introduces the .* and ->* operator for invoking pointers to
+    // member functions. These objects are larger than function
+    // pointers (since they need to store whether the function is
+    // virtual and handle virtual and multiple inheritance).
+    EXPECT_EQ(230, (obj.*ptr)(2, 3));
+    EXPECT_EQ(230, ((&obj)->*ptr)(2, 3));
+    // We can also use std::invoke, which explicitly passes the 'this'
+    // argument:
+    EXPECT_EQ(230, std::invoke(ptr, obj, 2, 3));
+    EXPECT_EQ(230, std::invoke(ptr, &obj, 2, 3));
+    // Functions that need a function argument should probably use
+    // std::function. We can create some type-erased versions of our
+    // function pointer, though the syntax is unbelievable:
+    std::function<int(int, int)> bound_ptr =
+        std::bind(&ClassWithVirtualFunction::f, &obj, std::placeholders::_1,
+                  std::placeholders::_2);
+    EXPECT_EQ(230, std::invoke(bound_ptr, 2, 3));
+    // Closure notation is arguably easier to read than std::bind with
+    // std::placeholders, but it is still a lot of syntax:
+    std::function<int(int, int)> alt_bound_ptr = [&obj](int a, int b) -> int {
+        return obj.f(a, b);
+    };
+    EXPECT_EQ(230, std::invoke(alt_bound_ptr, 2, 3));
+    // As far as I know, you can't just write &obj.f.
 }
 
 } // namespace
